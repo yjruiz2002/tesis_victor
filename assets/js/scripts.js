b7e9ae0a498d9d87fe1694421b1056d22d6cebc8
Yerlin Ruiz <yjruiz.20@est.ucab.edu.ve>
2024-01-30 01:21:46 -0400

    Agregar conexion de firebase con javascript a la pagina web

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -27,45 +27,6 @@
     };
     $(window).ready(e), $(window).on("resize", e);
 
-    /*================================
-    sidebar menu
-    ==================================*/
-    $("#menu").metisMenu();
-
-    /*================================
-    slimscroll activation
-    ==================================*/
-    $('.menu-inner').slimScroll({
-        height: 'auto'
-    });
-    $('.nofity-list').slimScroll({
-        height: '435px'
-    });
-    $('.timeline-area').slimScroll({
-        height: '500px'
-    });
-    $('.recent-activity').slimScroll({
-        height: 'calc(100vh - 114px)'
-    });
-    $('.settings-list').slimScroll({
-        height: 'calc(100vh - 158px)'
-    });
-
-    /*================================
-    stickey Header
-    ==================================*/
-    $(window).on('scroll', function() {
-        var scroll = $(window).scrollTop(),
-            mainHeader = $('#sticky-header'),
-            mainHeaderHeight = mainHeader.innerHeight();
-
-        // console.log(mainHeader.innerHeight());
-        if (scroll > 1) {
-            $("#sticky-header").addClass("sticky-menu");
-        } else {
-            $("#sticky-header").removeClass("sticky-menu");
-        }
-    });
 
     /*================================
     form bootstrap validation
@@ -109,13 +70,6 @@
 
 
     /*================================
-    Slicknav mobile menu
-    ==================================*/
-    $('ul#nav_menu').slicknav({
-        prependTo: "#mobile_menu"
-    });
-
-    /*================================
     login form
     ==================================*/
     $('.form-gp input').on('focus', function() {
@@ -126,48 +80,6 @@
             $(this).parent('.form-gp').removeClass('focused');
         }
     });
-
-    /*================================
-    slider-area background setting
-    ==================================*/
-    $('.settings-btn, .offset-close').on('click', function() {
-        $('.offset-area').toggleClass('show_hide');
-        $('.settings-btn').toggleClass('active');
-    });
-
-    /*================================
-    Owl Carousel
-    ==================================*/
-    function slider_area() {
-        var owl = $('.testimonial-carousel').owlCarousel({
-            margin: 50,
-            loop: true,
-            autoplay: false,
-            nav: false,
-            dots: true,
-            responsive: {
-                0: {
-                    items: 1
-                },
-                450: {
-                    items: 1
-                },
-                768: {
-                    items: 2
-                },
-                1000: {
-                    items: 2
-                },
-                1360: {
-                    items: 1
-                },
-                1600: {
-                    items: 2
-                }
-            }
-        });
-    }
-    slider_area();
 
     /*================================
     Fullscreen Page
